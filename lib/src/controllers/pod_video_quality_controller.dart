a018f6a8ccf30db4c4890c9490e15ad4cac43e50
Renat Shakhmatov <shushper@gmail.com>
2024-06-28 15:26:56 +0500

    Remove 240 quality limitation

--- lib/src/controllers/pod_video_quality_controller.dart
+++ lib/src/controllers/pod_video_quality_controller.dart
@@ -53,9 +53,6 @@ class _PodVideoQualityController extends _PodVideoController {
   ) {
     final urls0 = urls;
 
-    ///has issues with 240p
-    urls0?.removeWhere((element) => element.quality == 240);
-
     ///has issues with 144p in web
     if (kIsWeb) {
       urls0?.removeWhere((element) => element.quality == 144);
